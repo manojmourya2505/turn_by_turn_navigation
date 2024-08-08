699fce3e3373ac70e69ba965d29a8e06aa1e53c9
manojmourya2505 <mouryamanoj2505@gmail.com>
2024-08-08 16:05:53 +0530

    removed deprected issues

--- app/src/androidTest/java/com/inouiw/scrutiny/MainActivityTest.kt
+++ app/src/androidTest/java/com/inouiw/scrutiny/MainActivityTest.kt
@@ -3,19 +3,19 @@ package com.inouiw.scrutiny
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
     @get:Rule
-    var activityRule = ActivityTestRule(MainActivity::class.java)
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun buttonClickTesting() {
