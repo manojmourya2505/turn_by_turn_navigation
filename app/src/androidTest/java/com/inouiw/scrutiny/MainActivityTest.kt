090b9159516efb8dc65622f0f26d491269fb712e
manojmourya2505 <mouryamanoj2505@gmail.com>
2024-08-08 15:56:27 +0530

    removed error in calculator test file

--- app/src/androidTest/java/com/inouiw/scrutiny/MainActivityTest.kt
+++ app/src/androidTest/java/com/inouiw/scrutiny/MainActivityTest.kt
@@ -4,7 +4,6 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.hero.navigationtesting.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
